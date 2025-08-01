d504fab77204a9756ec0326eadd1134ca74913b7
Asim Patrick <asimokon@yahoo.com>
2025-08-01 17:40:24 +0100

    modified images

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('products/', views.productsViewAndCreate, name='products'),
     path('products/<slug:slug>/', views.productDetailBySlug,
